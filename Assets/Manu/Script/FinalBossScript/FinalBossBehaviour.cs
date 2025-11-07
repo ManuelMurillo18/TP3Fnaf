5b24e025c5eed0847f01bc181dbe37f89e746b8b
ManuelMurillo18 <manuel.d.murillo.2003@gmail.com>
2025-11-07 15:32:11 -0500

    yolo

--- Assets/Manu/Script/FinalBossScript/FinalBossBehaviour.cs
+++ Assets/Manu/Script/FinalBossScript/FinalBossBehaviour.cs
@@ -7,10 +7,7 @@ public class FinalBossBehaviour : BehaviorTree
     [SerializeField] GameObject playerTarget;
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] Transform[] mouvmentTargets;
-<<<<<<< HEAD
     [SerializeField] Transform releasePoint;
-=======
->>>>>>> parent of e33987d (bt)
     [SerializeField] float roamingRange = 20f;
     [SerializeField] Transform projectileReleasePoint;
     [SerializeField] float attackDuration = 5f;
@@ -27,7 +24,6 @@ public class FinalBossBehaviour : BehaviorTree
         interrupt = new Interrupt(this, seesPlayer);
         //interrupt = new Interrupt(this, attackCooldown);
 
-<<<<<<< HEAD
         FixedTargetMouvement movementTarget1 = new FixedTargetMouvement(agent, mouvmentTargets[0], 5, null, this);
         FixedTargetMouvement movementTarget2 = new FixedTargetMouvement(agent, mouvmentTargets[1], 5, null, this);
         FixedTargetMouvement movementTarget3 = new FixedTargetMouvement(agent, mouvmentTargets[2], 5, null, this);
@@ -41,24 +37,6 @@ public class FinalBossBehaviour : BehaviorTree
         //Wandering wandering = new Wandering(null, this,agent,roamingRange);
 
         root = new Selector(new Node[] {chaseSequence,walkingSequence,}, null, this);
-
-=======
-        FixedTargetMouvement firstMove = new FixedTargetMouvement(agent, mouvmentTargets[0], 5, null, this);
-        FixedTargetMouvement secondMove = new FixedTargetMouvement(agent, mouvmentTargets[1], 5, null, this);
-        FixedTargetMouvement thirdMove = new FixedTargetMouvement(agent, mouvmentTargets[2], 5, null, this);
-        FixedTargetMouvement fourthMove = new FixedTargetMouvement(agent, mouvmentTargets[3], 5, null, this);
-        FixedTargetMouvement fifthMove = new FixedTargetMouvement(agent, mouvmentTargets[4], 5, null, this);
-        //Wait waitBetweenMoves = new Wait(20f, null, this);
-        //TimedAttack attackAction = new TimedAttack(projectilePrefab, playerTarget.transform, projectileReleasePoint, attackDuration, null, this);
-        Chase chase = new Chase(null, this, agent,playerTarget,5);
-
-
-        Sequence movement = new Sequence(new Node[] { firstMove, secondMove, thirdMove, fourthMove, fifthMove }, null, this);
-        Selector attack = new Selector(new Node[] { chase }, seesPlayer, this);
-
-        // Parallel now automatically restarts both forever
-        root = new Selector(new Node[] { attack, movement }, null, this);
->>>>>>> parent of e33987d (bt)
     }
     private void OnDisable()
     {
